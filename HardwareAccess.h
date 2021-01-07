2b367795e261ac4d9d49aecea62bd3e36b733404
Patrick Grawehr <pgrawehr@hotmail.com>
2021-01-07 13:09:41 +0100

    The methods that access the hardware will go to a separate class
    To be able to later separately replace hardware dependent methods
    from low-level methods of the runtime.

---
+++ HardwareAccess.h
@@ -0,0 +1,22 @@
+﻿// HardwareAccess.h
+
+#ifndef _HARDWAREACCESS_h
+#define _HARDWAREACCESS_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "ConfigurableFirmata.h"
+#include "FirmataIlExecutor.h"
+
+class HardwareAccess
+{
+	MethodState ExecuteHardwareAccess(ExecutionState* currentFrame, NativeMethod method, const VariableVector& args, Variable& result);
+};
+
+
+#endif
+
