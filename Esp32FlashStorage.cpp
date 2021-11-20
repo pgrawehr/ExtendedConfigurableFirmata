3053c1cf0c4b82789cfeb6033bc20e82df5e2c95
Patrick Grawehr <pgrawehr@hotmail.com>
2021-11-20 15:00:15 +0100

    Intermediate commit: Try to read out the active firmware size
    - to check whether the firmware matches the flash space

--- Esp32FlashStorage.cpp
+++ Esp32FlashStorage.cpp
@@ -37,6 +37,17 @@ Esp32CliFlashStorage::~Esp32CliFlashStorage()
 	mapHandle = 0;
 }
 
+extern "C" unsigned char _etext;
+// See https://arduino.stackexchange.com/questions/83911/how-do-i-get-the-size-of-my-program-at-runtime/83916#83916 for
+// an explanation of this function
+long SizeOfApp()
+{
+	byte* rom_end = &_etext;
+	rom_end = (byte*)(((uint32_t)rom_end + 256) & ~0xFF); // Align to next free flash block (even if the memory ends right on a boundary)
+	return (long)rom_end;
+}
+
+
 
 void Esp32CliFlashStorage::PrintPartitions()
 {
@@ -59,6 +70,8 @@ void Esp32CliFlashStorage::PrintPartitions()
 	}
 
 	esp_partition_iterator_release(iter);
+
+	Firmata.sendStringf(F("Application size is %d bytes"), 4, SizeOfApp());
 
 	Firmata.sendStringf(F("Data partition of size %d mapped to memory address 0x%x"), 8, partition->size, mappedBaseAddress);
 }
